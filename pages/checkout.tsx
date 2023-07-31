292dfb09b20cc0ff68063b217106dc0f5c496374
mraf19 <raflimr1902@gmail.com>
2023-07-31 11:34:47 +0700

    handling private route

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -3,8 +3,14 @@ import Link from "next/link";
 import CheckoutDetail from "../components/organisms/CheckoutDetail";
 import CheckoutConfirmation from "../components/organisms/CheckoutConfirmation";
 import CheckoutItem from "../components/organisms/CheckoutItem";
+import jwtDecode from "jwt-decode";
+import { JWTPayloadTypes, UserTypes } from "../services/dataTypes";
 
-export default function Checkout() {
+type CheckOutProps = {
+  user: UserTypes;
+};
+
+export default function Checkout(props: CheckOutProps) {
   return (
     <>
       <section className="checkout mx-auto pt-md-100 pb-md-145 pt-30 pb-30">
@@ -28,4 +34,29 @@ export default function Checkout() {
       </section>
     </>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  const JWTToken = Buffer.from(token, "base64").toString("ascii");
+  const payload: JWTPayloadTypes = jwtDecode(JWTToken);
+  const user: UserTypes = payload.player;
+  const IMG = process.env.NEXT_PUBLIC_IMG_API;
+  user.avatar = `${IMG}/${user.avatar}`;
+
+  return {
+    props: {
+      user,
+    },
+  };
 }
