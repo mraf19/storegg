8c91f146662e59c76e88ef28d97fccab1c06264a
mraf19 <raflimr1902@gmail.com>
2023-08-03 11:15:50 +0700

    clean up code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,7 +31,6 @@ export default function Document() {
           integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
           crossOrigin="anonymous"
         ></script>
-        <title>StoreGG | Top Up and Gaming Store</title>
       </Head>
       <body>
         <Main />
